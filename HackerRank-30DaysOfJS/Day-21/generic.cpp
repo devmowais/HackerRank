2f84982a9e5df832c9a3f04b8ecbc8dbb41f8cd0
Muhammad Owais Sherwani <51533702+devmowais@users.noreply.github.com>
2022-11-22 11:04:27 +0500

    Day-21 (C++)

--- HackerRank-30DaysOfJS/Day-21/generic.cpp
+++ HackerRank-30DaysOfJS/Day-21/generic.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 /**
 *    Name: printArray
-*    Print each element of the generic vector on a new line. Do not return anything.
+*    Print each element of the generic vector on a new line. Do not return anything...
 *    @param A generic vector
 **/
 
