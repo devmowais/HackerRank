a1cfea4c5fe4b0c7d715f4b97cc52654cb46384d
Muhammad Owais Sherwani <51533702+devmowais@users.noreply.github.com>
2022-11-29 13:42:55 +0500

    Day-08

---
+++ HackerRank-10DaysOfJS/Day 8/buttonContainer/buttonsGrid.js
@@ -0,0 +1,20 @@
+function myFunction(){
+    let val4=document.getElementById('btn4').innerHTML;
+    let val1=document.getElementById('btn1').innerHTML;
+    let val2=document.getElementById('btn2').innerHTML;
+    let val3=document.getElementById('btn3').innerHTML;
+    let val6=document.getElementById('btn6').innerHTML;
+    let val9=document.getElementById('btn9').innerHTML;
+    let val8=document.getElementById('btn8').innerHTML;
+    let val7=document.getElementById('btn7').innerHTML;
+    
+    document.getElementById('btn1').innerHTML=val4;
+    document.getElementById('btn2').innerHTML=val1;
+    document.getElementById('btn3').innerHTML=val2;
+    document.getElementById('btn6').innerHTML=val3;
+    document.getElementById('btn9').innerHTML=val6;
+    document.getElementById('btn8').innerHTML=val9;
+    document.getElementById('btn7').innerHTML=val8;
+    document.getElementById('btn4').innerHTML=val7;
+  
+  }
