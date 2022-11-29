2364613ce900f47d4ea4a1401510f40d787b77e2
Muhammad Owais Sherwani <51533702+devmowais@users.noreply.github.com>
2022-11-29 12:25:46 +0500

    Day-03

---
+++ HackerRank-10DaysOfJS/Day 3/TrynCatch.js
@@ -0,0 +1,51 @@
+
+
+// <----------PROBLEM--------->
+// https://www.hackerrank.com/challenges/js10-try-catch-and-finally/problem?isFullScreen=true
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+        try {
+        let splitString = s.split("");
+        let reverseArray = splitString.reverse();
+        let joinArray = reverseArray.join("");
+        console.log(joinArray);
+    }
+    catch (error)
+        {
+            // Print s on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+            console.log(error.message);
+            console.log(s);
+        }
+    
+}
+
+
+
